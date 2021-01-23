9a4ca07a751323420fb0cb67502574f26b45c4fe
Loren Paulsen <lopaul@microsoft.com>
2021-01-23 06:21:17 +0000

    updating script

--- configure.sh
+++ configure.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
